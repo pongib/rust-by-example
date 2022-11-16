538304488dc72152c5309255c69076940f7a78f3
pongtsu <alwayshungrytolearn@gmail.com>
2022-11-16 13:42:48 +0700

    feat: Display and tuple.

--- 02-primitives/tuples.rs
+++ 02-primitives/tuples.rs
@@ -1,6 +1,36 @@
-fn reverse(pair: (i32, bool)) -> (bool, i32)
+use std::fmt;
 
 fn reverse(pair: (i32, bool)) -> (bool, i32) {
-  let (int_param, bool_param) = pair;
-  (bool_param, int_param)
+    let (int_param, bool_param) = pair;
+
+    (bool_param, int_param)
+}
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+fn main() {
+    let long_tuple = (
+        1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true,
+    );
+
+    println!("tuple first value {}", long_tuple.0);
+    println!("tuple second value {}", long_tuple.1);
+
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    println!("one element tuple: {:?}", (5u32,));
+    println!("value tuple: {:?}", (5u32));
+
+    let tuple = (1, "hello", 4.5, true);
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.3, 5.6);
+    println!("{:?}", matrix);
+
+    let simple_pair = (1, true);
+    println!("reverse {:?}", reverse(simple_pair));
 }

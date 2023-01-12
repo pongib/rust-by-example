266b390eefa7da299ab9abebd6ffe996658a60df
pongtsu <alwayshungrytolearn@gmail.com>
2023-01-12 16:17:43 +0700

    feat: Module.

---
+++ src/thebook/7.2-modules.rs
@@ -0,0 +1,33 @@
+// module grouping sample
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+// allow public child module and function
+mod front_of_house {
+    // need add pub for accessible
+    pub mod hosting {
+        // also add pub for function inside module
+        pub fn add_to_waitlist() {}
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
